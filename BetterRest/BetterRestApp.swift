8cc71daf3f0473205d72b489dce79b58039e9e8e
Jacob M <91759816+dimedevjake@users.noreply.github.com>
2023-01-22 06:07:03 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jacob and Shalise on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
